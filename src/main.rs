dbe7e77bd130ca681df22b7eadac4f3b93e72332
AdrianGerman <adrian.german1019@gmail.com>
2024-05-19 10:41:52 -0600

    conditionals

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,26 @@
 fn main() {
     // obtener el nombre del usuario
-    println!("Por favor introduce tu nombre: ");
-    let mut nombre: String = String::new();
-    std::io::stdin().read_line(&mut nombre).unwrap();
-    nombre = nombre.trim().to_string();
+    println!("Por favor introduce tu edad: ");
+    let mut edad: String = String::new();
+    std::io::stdin().read_line(&mut edad).unwrap();
 
-    // obtener el país del usuario
-    println!("Por favor introduce tu país: ");
-    let mut pais: String = String::new();
-    std::io::stdin().read_line(&mut pais).unwrap();
-    nombre = nombre.trim().to_string();
+    // convertir la edad a enteros
+    let edad_int: u8 = edad.trim().parse().unwrap();
 
-    println!("Hola, bienvenido/a {} de {}", nombre, pais);
+    if edad_int >= 18 && edad_int != 30 {
+        println!("Puedas entrar a la discoteca");
+    } else if edad_int == 30 {
+        println!("No admitimos personas exactamente de 30 años");
+    } else {
+        println!("Eres menos de edad, todavía no puedes entrar");
+    }
+
+    // if edad_int >= 18 {
+    //     println!("Puedes entrar a la discoteca");
+    // } else {
+    //     println!("No puedes entrar a la discoteca")
+    // }
+
+    println!("Tienes {} años", edad_int);
+
 }
