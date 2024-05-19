13c07e6fab1380a11d0416435d8eaa4fc51b7005
AdrianGerman <adrian.german1019@gmail.com>
2024-05-19 08:51:32 -0600

    receiving user data

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
 fn main() {
-    let nombre: &str = "Adrian";
-    let edad: u8 = 22;
-    println!("Hola soy {} y tengo {} años", nombre, edad);
+    println!("Por favor introduce tu nombre: ");
+
+    let mut nombre: String = String::new();
+    std::io::stdin().read_line(&mut nombre).unwrap();
+    nombre = nombre.trim().to_string();
+
+    // obtener la edad
+    println!("Por favor introduce tu edad: ");
+    let mut edad: String = String::new();
+    std::io::stdin().read_line(&mut edad).unwrap();
+
+    // convertir la edad a numero
+    let edad_int: u8 = edad.trim().parse().unwrap();
+
+
+    println!("Hola, bienvenido/a {} de {} años", nombre, edad_int);
 }
