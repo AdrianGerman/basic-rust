54c54ae499f5a0a27341fbf1939a83394e0f7d13
AdrianGerman <adrian.german1019@gmail.com>
2024-05-21 09:34:54 -0600

    environment description

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
 mod calculator;
+
+use csv::{ReaderBuilder, StringRecord};
+use std::{fs};
+
+const FILENAME: &str = "history.csv";
+
 fn main() {
-    calculator::run();
+    // calculator::run();
+
+    let content = fs::read_to_string(FILENAME).unwrap();
+    let mut rdr = ReaderBuilder::new().delimiter(b';').from_reader(content.as_bytes());
+
+    for result in rdr.records() {
+        println!("Texto: {}", result.unwrap().get(2).unwrap().trim());
+    }
 }
